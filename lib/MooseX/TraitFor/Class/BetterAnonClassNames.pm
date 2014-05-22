4b2e7093caee526268925ac35bfc878232689403
Chris Weyl <cweyl@alumni.drew.edu>
2014-05-21 17:09:11 -0700

    initial commit

---
+++ lib/MooseX/TraitFor/Class/BetterAnonClassNames.pm
@@ -0,0 +1,19 @@
+package MooseX::TraitFor::Class::BetterAnonClassNames;
+
+# ABSTRACT: The great new MooseX::TraitFor::Class::BetterAnonClassNames!
+
+use Moose;
+use namespace::autoclean;
+
+
+__PACKAGE__->meta->make_immutable;
+!!42;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 SEE ALSO
+
+=cut
